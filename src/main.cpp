14fab6428e819a9d53c5393fab0e600d80b316be
Anurag <anuragbhatt865@gmail.com>
2023-08-17 01:21:34 +0530

    Baisc changes, press 'c' to clear everything

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,8 @@ bool ValidIndex(int,int);
 void SetCellAlive(Cell*);
 void SetCellDead(Cell*);
 
+void CheckForAliveCells();
+
 
 int main()
 {
@@ -37,7 +39,7 @@ int main()
             for (int j = 0; j < Rows; j++)
             {
                 // Draw Rectangles
-                grid[i][j] =  Cell{i,j},false;
+                grid[i][j] =  Cell{i,j,false};
             }
             
         }
@@ -49,38 +51,7 @@ int main()
     while (!WindowShouldClose())
     {
 
-        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-
-            Vector2 mPos = GetMousePosition();
-            int xIndex = mPos.x/cellWidth;
-            int yIndex = mPos.y/cellHeight;
-            
-            std::cout << "X position: " << xIndex << "Y position: " << yIndex << std::endl;
-
-            if(ValidIndex(xIndex,yIndex)){
-
-                SetCellAlive(&grid[xIndex][yIndex]);
-                
-            }
-
-        }
-
-        if(IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)){
-
-            Vector2 mPos = GetMousePosition();
-            int xIndex = mPos.x/cellWidth;
-            int yIndex = mPos.y/cellHeight;
-            
-            std::cout << "X position: " << xIndex << "Y position: " << yIndex << std::endl;
-
-            if(ValidIndex(xIndex,yIndex)){
-
-                if(grid[xIndex][yIndex].isAlive == true)
-                      SetCellDead(&grid[xIndex][yIndex]);
-            }
-
-        }
-
+        CheckForAliveCells();
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
@@ -105,7 +76,7 @@ int main()
 void DrawCell(Cell cell){
     
     if(cell.isAlive == true)
-        DrawRectangle(cell.xPos * cellWidth,cell.yPos * cellHeight, cellWidth, cellHeight, GREEN);
+        DrawRectangle(cell.xPos * cellWidth,cell.yPos * cellHeight, cellWidth, cellHeight, PINK);
     else
         DrawRectangleLines(cell.xPos * cellWidth,cell.yPos * cellHeight, cellWidth, cellHeight, BLACK);
 }
@@ -124,4 +95,50 @@ void SetCellAlive(Cell *cell){
 void SetCellDead(Cell *cell){
 
     cell->isAlive = false;
+}
+
+void CheckForAliveCells(){
+
+
+    Vector2 mPos = GetMousePosition();
+    int xIndex = mPos.x/cellWidth;
+    int yIndex = mPos.y/cellHeight;
+
+    if(IsKeyPressed(KEY_C)){
+
+        for (int i = 0; i < Cols; i++)
+        {
+            for (int j = 0; j < Rows; j++)
+            {
+               SetCellDead(&grid[i][j]);
+            }
+            
+        }
+
+    }
+
+
+    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+        
+    std::cout << "X position: " << xIndex << "Y position: " << yIndex << std::endl;
+
+     if(ValidIndex(xIndex,yIndex)){
+
+        SetCellAlive(&grid[xIndex][yIndex]);
+                
+     }
+     }
+
+    if(IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)){
+  
+    std::cout << "X position: " << xIndex << "Y position: " << yIndex << std::endl;
+
+    if(ValidIndex(xIndex,yIndex)){
+
+    if(grid[xIndex][yIndex].isAlive == true)
+        SetCellDead(&grid[xIndex][yIndex]);
+    }
+
+    }
+
 }
